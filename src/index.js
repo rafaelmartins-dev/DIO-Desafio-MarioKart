21b3cd062075d329a7e423b7ab0354d8f8e5f1bb
Rafael Martins <rafaellmartins.dev@gmail.com>
2026-03-13 19:17:03 -0300

    incluir todos os players

--- src/index.js
+++ src/index.js
@@ -14,6 +14,38 @@ const player2 = {
     pontos: 0
 };
 
+const player3 = {
+    nome: "Peach",
+    velocidade: 3,
+    manobrabilidade: 4,
+    poder: 2,
+    pontos: 0
+}
+
+const player4 = {
+    nome: "Bowser",
+    velocidade: 5,
+    manobrabilidade: 2,
+    poder: 5,
+    pontos: 0
+}
+
+const player5 = {
+    nome: "Yoshi",
+    velocidade: 2,
+    manobrabilidade: 4,
+    poder: 3,
+    pontos: 0
+}
+
+const player6 = {
+    nome: "Donkey Kong",
+    velocidade: 2,
+    manobrabilidade: 2,
+    poder: 5,
+    pontos: 0
+}
+
 
 async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
